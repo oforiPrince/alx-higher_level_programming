1bfbd7d1460a0e6580cbfb4f9d95e020d2230915
ofori_Prince <Pofori034@st.ug.edu.gh>
2022-12-08 05:15:48 +0000

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/103-fast_alphabet.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-import string
-print(strings.ascci upper)`:
--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    result = 0
-    import sys
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print("{}".format(result))
